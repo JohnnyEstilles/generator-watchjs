705f0a723c0a33c87dcee5cab02a35ac040f1923
Tomasz Ducin <tomasz.ducin@gmail.com>
2015-04-19 18:45:34 +0200

    sorting speakers file, improved console output

--- speaker/index.js
+++ speaker/index.js
@@ -1,9 +1,11 @@
 'use strict';
 
-var fs = require('fs');
 var yeoman = require('yeoman-generator');
-var slug = require('slug');
+var chalk = require('chalk');
+
 var _ = require('lodash');
+var fs = require('fs');
+var slug = require('slug');
 
 module.exports = yeoman.generators.Base.extend({
   initializing: function () {
@@ -24,7 +26,7 @@ module.exports = yeoman.generators.Base.extend({
     }], function (answers) {
       this.options.file = answers.file;
       var generatedSlug = slug(answers.name, {lower: true});
-      this.log('Generated slug is: ' + generatedSlug);
+      this.log('Generated slug is: ' + chalk.yellow.bold(generatedSlug));
       this.options.speaker = {
         id: generatedSlug,
         name: answers.name
@@ -36,23 +38,26 @@ module.exports = yeoman.generators.Base.extend({
   writing: function () {
     function str2json(value){ return JSON.stringify(value, null, 4); }
     var newSpeaker = this.options.speaker;
-    this.log('Trying to add: ' + str2json(newSpeaker));
+    this.log('Trying to add: ' + str2json(newSpeaker) + "\n");
     var content = JSON.parse(fs.readFileSync(this.options.file, 'utf8'));
-    this.log('Reading ' + this.options.file + ' file, found ' + content.length + ' elements.');
+    this.log('Reading ' + chalk.yellow.bold(this.options.file) + ' file, found ' + content.length + ' elements.');
 
     var sameIdElement = _.find(content, function(el){ return el.id === newSpeaker.id; });
     if (sameIdElement) {
-      this.log('Element with the same `id` exists:' + str2json(sameIdElement));
+      this.log(chalk.red('Element with the same `id` exists:' + str2json(sameIdElement)));
     }
 
     var sameNameElement = _.find(content, function(el){ return el.name === newSpeaker.name; });
     if (sameNameElement) {
-      this.log('Element with the same `name` exists:' + str2json(sameNameElement));
+      this.log(chalk.red('Element with the same `name` exists:' + str2json(sameNameElement)));
     }
 
     if (!sameIdElement && !sameNameElement) {
-      this.log('Similar element hasn\'t been found, appending to file.');
+      this.log('Similar element hasn\'t been found.');
       content.push(newSpeaker);
+      this.log('Appending to collection and sorting');
+      content = _.sortBy(content, function(el){ return el.id; });
+      this.log('Rewriting file.');
       fs.writeFileSync(this.options.file, str2json(content));
       this.log('Done.');
     }
