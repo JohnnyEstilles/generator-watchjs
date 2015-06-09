b4c1d0cbc5191695c6da0016eb65b2d5354859d6
Johnny Estilles <johnny.estilles@agentia.asia>
2015-06-09 17:17:11 +0800

    fix init test

--- test/test-speaker.js
+++ test/test-speaker.js
@@ -3,18 +3,28 @@
 var path = require('path');
 var assert = require('yeoman-generator').assert;
 var helpers = require('yeoman-generator').test;
+var inspect = require('util').inspect;
 
-describe('Watchjs:speaker', function () {
+describe('watchjs:speaker', function () {
   before(function (done) {
+    var self = this;
+    var name = 'John Doe';
+    var testPath = path.join(__dirname, 'temp');
+
+    // store in test obejct for later use
+    this.filePath = path.join(testPath, 'speaker.json');
+
     helpers.run(path.join(__dirname, '../speaker'))
-      .withArguments('name', '--force')
+      .inDir(testPath)
+      .withPrompts({ 'file': self.filePath, 'name': name })
       .withOptions({ 'skip-install': true })
       .on('end', done);
   });
 
   it('creates files', function () {
-    assert.file([
-      'somefile.js'
-    ]);
+    assert.file(this.filePath);
+    assert.fileContent(this.filePath, /\"id\":.*\"john-doe\"/);
+    assert.fileContent(this.filePath, /\"name\":.*\"John Doe\"/);
   });
+
 });
